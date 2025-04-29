f6ad251bd955ed8de32d781988e27f37c4be72d1
iago <iago.cesar@live.com>
2025-04-29 20:44:10 -0300

    add metadata and some assets

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,12 @@
 import '@/assets/styles/globals.css';
 import type { ReactNode } from 'react';
 
+export const metadata = {
+  title: 'Property Pulse',
+  keywords: 'rental, property, real estate',
+  description: 'Find the perfect rental property'
+}
+
 const MainLayout = ({ children }: { children: ReactNode }) => {
   return (
     <html>
