6b0fab4f68b30633657f6f08f1a2a653ea680956
iago <iago.cesar@live.com>
2025-05-02 19:53:58 -0300

    add property detail page

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -1,10 +1,46 @@
+import connectDB from "@/config/database";
+import Property, { IProperty } from "@/models/Property";
+import PropertyHeaderImage from "@/components/PropertyHeaderImage";
+import Link from "next/link";
+import { FaArrowLeft } from "react-icons/fa";
+import PropertyDetails from "@/components/PropertyDetails";
+
 const PropertyPage = async ({
   params
 }: {
   params: Promise<{ id: string }>
 }) => {
+  await connectDB();
   const { id } = await params;
-  return (<div>Property Page {id}</div>);
+  const property: IProperty | null = await Property.findById(id).lean();
+
+  if (property === null) {
+    console.error("Property not found");
+    return;
+  }
+
+  return (
+    <>
+      <PropertyHeaderImage image={property.images[0]} />
+      <section>
+        <div className="container m-auto py-6 px-6">
+          <Link
+            href="/properties"
+            className="text-blue-500 hover:text-blue-600 flex items-center"
+          >
+            <FaArrowLeft className="mr-2" /> Back to Properties
+          </Link>
+        </div>
+      </section>
+      <section className="bg-blue-50">
+        <div className="container m-auto py-10 px-6">
+          <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
+            <PropertyDetails property={property} />
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default PropertyPage;
